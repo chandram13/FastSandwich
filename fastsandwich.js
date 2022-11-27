a01673545ad3985f17a7c7ae4dd138f63ae4a474
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-27 06:54:55 -0800

    Add files via upload

---
+++ fastsandwich.js
@@ -0,0 +1,32 @@
+// Marvish Chandra
+
+class favoriteSandwiches{
+    function varietyMeals(){
+        seasonalSpecials = {"Assorted Box Lunch Meal Bundle": 143.76, "Classic Combo Sandwich Platter Bundle": 110.14};
+        ezOrderPlatters = {"Easy Order Sandwich Platter": 53.99, "Easy Order Wrap Platter": 63.99};
+        ezOrderSandwichBoxMeals = {"Turkey Breast Sandwich", "Black Forest Ham Sandwich", "Italian B.M.T. Sandwich", "Cold Cut Combo Sandwich", "Spicy Italian Sandwich", "Tuna Sandwich", "Roast Beef Sandwich": 8.99};
+    function wrapBoxMeals(){
+        signatureWraps = {"Turkey Breast","Veggie Delite","Italian B.M.T","Black Forest Ham", "Tuna", "Spicy Italian", "Cold Cut Combo", "Roast Beef": 13.39};
+    }
+    function customPlatters(){
+        customizeSub = 58.99;
+        customizeWrap = 68.99;
+    }
+    function sandwichBoxes(){
+        sandwichBoxmeals = {"Turkey Breast","Black Forest Ham","Italian B.M.T","Veggie Delite","Spicy Italian","Cold Cut Combo","Tuna","Roast Beef": 9.49};
+    }
+    function subwaySides(){
+        individualChips = 1.49;
+        toppingsPlatter = 16.99;
+    }
+    function subwayDesserts(){
+        dozenCookies = 8.39;
+        cookiePlatter = 23.49;
+    }
+    function subwayBeverages(){
+        bottledBeverages = 2.49;
+        gallonLemonade = 8.09;
+        bottledCola = 2.49;
+    }
+    }
+}
